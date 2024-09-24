18214470777832294b68f404834b5bebff3c307b
Michelle Weidling <weidling@sub.uni-goettingen.de>
2024-09-24 13:07:14 +0200

    feat: remove keys with empty fields

--- src/commons.py
+++ src/commons.py
@@ -10,8 +10,20 @@ def remove_mongodb_id_from_result(json_data, result_type) -> list:
     purged_list = []
     for obj in json_data:
         d = dict(obj)
+        remove_keys_from_dict(d)
         del d['_id']
         if result_type == 'gt':
             d = d['gt_workspace']
         purged_list.append(d)
     return purged_list
+
+def remove_keys_from_dict(d: dict) -> dict:
+    """
+    This function removes keys with the value of null from the result.
+    """
+    if isinstance(d, dict):
+        for key in list(d.keys()):
+            if d[key] == None:
+                del d[key]
+            else:
+                remove_keys_from_dict(d[key])
